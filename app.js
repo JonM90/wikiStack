60b8470b29b6cd978a59d873333d16bebe22644f
Jonathan Martinez <jonm90@gmail.com>
2017-09-25 10:09:27 -0400

    added to template

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ const express = require('express');
 const app = express();
 const routes = require('./routes');
 const bodyParser = require('body-parser');
-const urlencodedParser = bodyParser.urlencoded({ extended: true });
+const nunjucks = require('nunjucks')
 
 app.use(express.static('public'));
 
 app.use(morgan('tiny')); // Log request details before passing control over to routes
-app.use(urlencodedParser);
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use('/', routes());
+var env = nunjucks.configure('views', {noCache: true});
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render);// when res.render works with html files, have it use nunjucks to do so
+
+app.use('/', routes);
 
 app.listen(3000, function() {
   console.log('Listening on port 3000');
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-// -------------RULES FOR '/routes/index.js' MODULE.EXPORTS object ------------
-// Save all routes in one big function that gets exported and require in app.js
-// Should return router object
-// ----------------------------------------------------------------------------
-module.exports = function () {
-  router.get('/', function(req, res) {
-    res.send('WIKISTACK HOME');
-  });
-  router.get('/jonathan', function(req, res) {
-    res.send('Jonathan: co founder of wikistack');
-  });
-  router.get('/devon', function(req, res) {
-    res.send('Devon: also co founder of wikistack');
-  });
-  return router;
-};
+router.get('/', function(req, res) {
+  res.send('WIKISTACK HOME');
+});
+router.get('/jonathan', function(req, res) {
+  res.send('Jonathan: co founder of wikistack');
+});
+router.get('/devon', function(req, res) {
+  res.send('Devon: also co founder of wikistack');
+});
+
+module.exports = router;
